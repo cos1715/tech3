b858a8ea9a8abc02f8f038cb35d36af2e07e1d65
Taras <taras.samoilenko@globaltechmakers.com>
2019-08-06 15:57:44 +0300

    Updates

---
+++ precache-manifest.26d2bded0f2b2bd5df9bb7672e09fefc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a741083d95958b5d12664046b8f9cf",
+    "url": "/tech3/index.html"
+  },
+  {
+    "revision": "fc753d2d6903c3d26eb4",
+    "url": "/tech3/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e918478e2572e9e02e60",
+    "url": "/tech3/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "fc753d2d6903c3d26eb4",
+    "url": "/tech3/static/js/2.2ff8189d.chunk.js"
+  },
+  {
+    "revision": "e918478e2572e9e02e60",
+    "url": "/tech3/static/js/main.c228c98f.chunk.js"
+  },
+  {
+    "revision": "a851681ecfea4fd51566",
+    "url": "/tech3/static/js/runtime~main.57b35e41.js"
+  },
+  {
+    "revision": "72b4f90902954721ca5d45a528c86777",
+    "url": "/tech3/static/media/me.72b4f909.jpg"
+  }
+]);
